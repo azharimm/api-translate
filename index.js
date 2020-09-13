e67969577e76a0cf1b03e0fdfc53de14067a6f1a
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-09-13 07:18:51 +0700

    ^ cors

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const indexRoute = require('./routes/index');
+const indexRoute = require("./routes/index");
 
+//cors
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://translate.azharimm.tk');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+});
 
-app.use('/', indexRoute);
+//routes
+app.use("/", indexRoute);
 
 //listen
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
-    console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-    )
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+	)
 );
